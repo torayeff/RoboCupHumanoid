95aa9b63ade3e9a839cba83496d3c5c930f6cc93
torayeff <torayeff@gmail.com>
2019-01-22 22:07:44 +0100

    Automatic labeling

--- test.py
+++ test.py
@@ -24,13 +24,17 @@ optimizer = torch.optim.Adam(model.parameters())
 trainset = utils.SoccerBallDataset("data/train/data.csv", "data/train", downsample=downsample)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
 
-model = SweatyNet1()
-model.load_state_dict(torch.load("pretrained_models/epoch_90.model", map_location='cpu'))
-model.eval()
-metrics = utils.evaluate_model(model, device, trainset, verbose=True)
-print(metrics)
+for data in trainset:
+    print(data)
 
-rc = metrics['tps']/(metrics['tps'] + metrics['fns'])
-fdr = metrics['fps']/(metrics['fps'] + metrics['tps'])
-
-print("RC: {}, FDR: {}".format(rc, fdr))
+    break
+# model = SweatyNet1()
+# model.load_state_dict(torch.load("pretrained_models/epoch_100.model", map_location='cpu'))
+# model.eval()
+# metrics = utils.evaluate_model(model, device, trainset, verbose=True)
+# print(metrics)
+#
+# rc = metrics['tps']/(metrics['tps'] + metrics['fns'])
+# fdr = metrics['fps']/(metrics['fps'] + metrics['tps'])
+#
+# print("RC: {}, FDR: {}".format(rc, fdr))

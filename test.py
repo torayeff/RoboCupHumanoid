7f7847fa6d1d04c0fd6c7eb5179c1c9db58f9a66
torayeff <torayeff@gmail.com>
2019-01-25 21:59:07 +0100

    nothing

--- test.py
+++ test.py
@@ -15,8 +15,8 @@ downsample = 4
 batch_size = 4
 
 model = SweatyNet1()
-# model.load_state_dict(torch.load("pretrained_models/epoch_20.model", map_location='cpu'))
-model.to(device)
+model.load_state_dict(torch.load("pretrained_models/epoch_100.model", map_location='cpu'))
+model.eval()
 
 criterion = nn.MSELoss()
 optimizer = torch.optim.Adam(model.parameters())
@@ -24,10 +24,6 @@ optimizer = torch.optim.Adam(model.parameters())
 trainset = utils.SoccerBallDataset("data/train/data.csv", "data/train", downsample=downsample)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
 
-
-model = SweatyNet1()
-model.load_state_dict(torch.load("pretrained_models/epoch_100.model", map_location='cpu'))
-model.eval()
 metrics = utils.evaluate_model(model, device, trainset, verbose=True)
 print(metrics)
 

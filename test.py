714c751e7140eb661f2c5dea21b1b7000bc3b333
torayeff <torayeff@gmail.com>
2019-01-27 11:32:43 +0100

    Playing with test

--- test.py
+++ test.py
@@ -14,24 +14,21 @@ print(device)
 downsample = 4
 batch_size = 4
 
-model = SweatyNet1()
-# model.load_state_dict(torch.load("pretrained_models/epoch_20.model", map_location='cpu'))
-model.to(device)
+# model = SweatyNet1()
+# model.load_state_dict(torch.load("pretrained_models/epoch_100.model", map_location='cpu'))
+# model.to(device)
+#
+# criterion = nn.MSELoss()
+# optimizer = torch.optim.Adam(model.parameters())
 
-criterion = nn.MSELoss()
-optimizer = torch.optim.Adam(model.parameters())
-
-trainset = utils.SoccerBallDataset("data/train/data.csv", "data/train", downsample=downsample)
+trainset = utils.SoccerBallDataset("data/train_images/data.csv", "data/train_images", downsample=downsample)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
 
-
-model = SweatyNet1()
-model.load_state_dict(torch.load("pretrained_models/epoch_100.model", map_location='cpu'))
-model.eval()
-metrics = utils.evaluate_model(model, trainset, verbose=True)
-print(metrics)
-
-rc = metrics['tps']/(metrics['tps'] + metrics['fns'])
-fdr = metrics['fps']/(metrics['fps'] + metrics['tps'])
-
-print("RC: {}, FDR: {}".format(rc, fdr))
+# model.eval()
+model = None
+metrics = utils.evaluate_model(model, device, trainset, verbose=True, debug=True)
+#
+# rc = metrics['tps']/(metrics['tps'] + metrics['fns'])
+# fdr = metrics['fps']/(metrics['fps'] + metrics['tps'])
+#
+# print("RC: {}, FDR: {}".format(rc, fdr))
